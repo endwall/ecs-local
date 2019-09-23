74bd7b2ac9cb3a304da781b238a006ffdb28afb6
endwall <endwall@local>
2019-09-23 11:57:02 +1000

    add init helloworld app and dockerfile

---
+++ README.md
@@ -0,0 +1,16 @@
+# localized-ecs
+A Localised ECS Test Example
+
+## Hello World Sample App
+### Run without docker
+```
+npm install
+npm start
+```
+App port can be overwritten, for example:
+```
+PORT=8080 npm start
+```
+It will expose 8080 locally via http://localhost:8080
+
+## Test Container with AWS Credentails Locally
---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = 80;
+if (process.env.PORT) {
+	port = process.env.PORT
+}
+
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
